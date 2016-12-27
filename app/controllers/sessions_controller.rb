ae4f205c490a72b95b6d2423e43520cc0e1b27b6
prettyOOvi <thanhvi0905@gmail.com>
2016-12-27 13:27:00 +0700

    Sign Up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_back_or user
+    else
+      flash.now[:danger] = t "error_invalid"
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Fels219
@@ -15,11 +15,11 @@ module Fels219
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = Settings.time_zone
+   
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = 'blipblapblup'
     # Do not swallow errors in after_commit/after_rollback callbacks.
   end
 end
