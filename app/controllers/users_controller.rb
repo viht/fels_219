513cd2b987ce81b9869b1effff29ba70e3e9d423
prettyOOvi <thanhvi0905@gmail.com>
2016-12-27 13:27:00 +0700

    Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,4 +30,17 @@ class UsersController < ApplicationController
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t"let_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by params[:id]
+    redirect_to(root_url) unless current_user? @user
+  end
 end
